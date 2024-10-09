8acd15b93d235c4e79019fdf3d49c226e0e8e2a7
RaoBoTic <navinishrao@gmail.com>
2024-10-09 14:59:37 +0800

    updated

---
+++ styles/assets/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('mobile'); // Toggle the 'mobile' class
+});
